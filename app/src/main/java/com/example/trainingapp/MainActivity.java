a4a924d0a5aee917bb97abc723d9804e277219a6
Dawid Pieciul <pieciul.dawid@gmail.com>
2022-01-26 11:49:32 +0100

    Strona główna wszytskie kafelki + Aktiviti do każdego z nich

--- app/src/main/java/com/example/trainingapp/MainActivity.java
+++ app/src/main/java/com/example/trainingapp/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.trainingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pull(View view) {
+
+        Intent intent = new Intent(MainActivity.this, PullActivity.class);
+        startActivity(intent);
+    }
+
+    public void push(View view) {
+        Intent intent = new Intent(MainActivity.this, PushActivity.class);
+        startActivity(intent);
+    }
+
+    public void abs(View view) {
+        Intent intent = new Intent(MainActivity.this, AbsActivity.class);
+        startActivity(intent);
+    }
+
+    public void legs(View view) {
+        Intent intent = new Intent(MainActivity.this, LegsActivity.class);
+        startActivity(intent);
+    }
+
+    public void run(View view) {
+        Intent intent = new Intent(MainActivity.this, RunActivity.class);
+        startActivity(intent);
     }
 }
