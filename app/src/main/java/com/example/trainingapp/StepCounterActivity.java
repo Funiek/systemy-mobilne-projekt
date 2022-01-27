69b407a0ff8b7207e319699226b9299505235bbf
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2022-01-27 13:36:28 +0100

    zaimplementowanie krokomierza

--- app/src/main/java/com/example/trainingapp/StepCounterActivity.java
+++ app/src/main/java/com/example/trainingapp/StepCounterActivity.java
@@ -2,13 +2,72 @@ package com.example.trainingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.TextView;
 
-public class StepCounterActivity extends AppCompatActivity {
+public class StepCounterActivity extends AppCompatActivity implements SensorEventListener {
+    private TextView tvStepCounter;
+    private SensorManager sensorManager;
+    private Sensor sensor;
+    private boolean isCounterSensorPresent;
+    int stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_step_counter);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        tvStepCounter = findViewById(R.id.stepCounterValue);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
+            sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            tvStepCounter.setText(String.valueOf(stepCount));
+            isCounterSensorPresent = true;
+        }
+        else {
+            tvStepCounter.setText("Brak czujnika. KUP NOWY TELEFON :D");
+            isCounterSensorPresent = false;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == sensor) {
+            Log.d("dsadsa","sad");
+            stepCount = (int) event.values[0];
+            tvStepCounter.setText(String.valueOf(stepCount));
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
+            sensorManager.unregisterListener(this, sensor);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!=null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+
     }
 }
